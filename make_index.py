ea60514193b6bba09e1024084ec1cb19ec8c375c
nch <nichdel@gmail.com>
2017-05-25 12:54:52 -0500

    Integrate header

--- make_index.py
+++ make_index.py
@@ -10,7 +10,14 @@ links.remove(next)
 links.sort()
 links.reverse()
 
+head = open('Header/header.html', 'r')
+
 f = open('index.md', 'w')
+
+for line in head:
+   f.write(line.replace('__ACTIVE_ADOP__', 'active'))
+
+head.close()
 
 f.write("[The next (unofficial) report](" + fold +  next + ") \n\n")
 f.write("[The latest report](" + fold + links.pop(0) + ") \n\n")
